b76b572683c5068d019ef84b466239c48fd9498b
Şafak YILDIZ <shafakyildiz@gmail.com>
2021-05-29 19:41:13 +0300

    edit js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // The movie database API Key
 
-const API_KEY = "1b5484d18124e1c0063908920ff16f12";
+const API_KEY = "api_key=1b5484d18124e1c0063908920ff16f12";
 const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc';
+const DISCOVER =  '/discover/movie?sort_by=popularity.desc&';
+const API_URL = BASE_URL + DISCOVER + API_KEY;
