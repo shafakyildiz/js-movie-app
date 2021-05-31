8efebf3bb416d4c93a45a2e7cf92f980d351188f
Şafak YILDIZ <shafakyildiz@gmail.com>
2021-05-31 08:56:14 +0300

    added search functionality

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const DISCOVER = "/discover/movie?sort_by=revenue.desc&";
 const API_URL = BASE_URL + DISCOVER + API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
+const searchURL = BASE_URL + "/search/movie?" + API_KEY;
 const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
 getMovies(API_URL);
 
 // Fetching the data from API
@@ -61,3 +65,14 @@ function getColor(vote) {
     return "red";
   }
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    getMovies(searchURL + "&query=" + searchTerm);
+  } else {
+    getMovies(API_URL);
+  }
+});
